2882c76d9ace5909e002b36daa4af0a9708604b4
MVV1983 <silf083@gmail.com>
2020-11-13 11:55:35 +0700

    add new class check add field name

--- Vector/src/Check.java
+++ Vector/src/Check.java
@@ -1,3 +1,15 @@
 public class Check {
     private String name = "";
+
+    public Check(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- Vector/src/Main.java
+++ Vector/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        Check goose = new Check("Гусь");
+        System.out.println(goose.getName());
     }
 }
